203b29bc5791f3cd563b550b47f6acc02350cf88
RessChild <39401832+RessChild@users.noreply.github.com>
2020-03-28 13:30:23 +0900

    [수정] 분산처리
    No1009
    - 이전에 성공했다고 표기했으나, 실패했던 코드 재풀이
    
    성공

--- No1009.cpp
+++ No1009.cpp
@@ -1,32 +1,23 @@
-// 분산 처리
-// math 함수 내의 pow값을 사용할 생각이었으나
-// long long 타입으로도 오버플로우가 나버려 포기
-// 1의 자리만 사용하는 알고리즘
-
 #include<iostream>
 using namespace std;
+// 분산처리
+
+#define DIV 10 
+
+
+int T, A, B;
 
 int main() {
-	int loop; //반복 횟수
-	cin >> loop;
+	cin >> T;
+	for (int k = 0; k < T; k++) {
+		cin >> A >> B;
 
-	int *ans = new int[loop]; //동적할당
-
-	for (int k = 0; k < loop; k++) {
-		int sub, pow;
-		cin >> sub >> pow; // 값과 지수 입력
-
-		ans[k] = sub; // 첫 값은 sub 값 그대로
-		for (int i = 1; i < pow; i++) {
-			ans[k] = (ans[k] * sub) % 10 != 0 ? (ans[k] * sub) % 10 : 10;
-			//1의 자릿수만 알면 충분
-			//10으로 나눠서 딱떨어지면 10번째 컴퓨터가 처리하니 예외처리
+		int ans = 1;
+		for (int k = 1; k <= B; k++) {
+			ans *= A;
+			ans = ans % DIV;
 		}
+		cout << (ans ? ans : 10) << endl;
+		// 1~9 까지는 문제가 없으나, 0은 10번 기계를 의미
 	}
-
-	for (int k = 0; k < loop; k++) {
-		cout << ans[k] << endl;
-	}
-
-	delete[] ans; //할당 해제
 }
