f91da79aafde06bbcc9d251be8a30cb146d33e48
RessChild <39401832+RessChild@users.noreply.github.com>
2020-03-28 12:08:04 +0900

    [수정] 여행 가자
    No1976
    - 이전 코드는 일종의 완전탐색으로 시간초과 문제가 발생했으나,
    Union-Find 알고리즘을 사용하여 문제를 해결
    
    성공

--- No1976.cpp
+++ No1976.cpp
@@ -1,61 +1,47 @@
 #include<iostream>
 using namespace std;
 // 여행 가자
-// 도시끼리 연결이 되어야 계획대로 움직일 수 있다는 점에 초점을 둠
-// so, 각 도시가 하나의 유니온으로 이루어져 있는지만을 판단
+// 완전 탐색 문제처럼 보이지만, 연결만 존재한다면 경로를 크게 돌아서 갈 수 있음
+// so, 실제로는 그룹핑 문제이며 Union-Find 로 해결 가능
 
-class Link { //도시의 연결 정보
-public:
-	Link * link;
-	Link();
-};
+#define NUM 200
 
-int city, schedule;
-Link Bridge[200];
+int N, M, group[NUM + 1];
+
+int Find(int a) {
+	if (group[a] == a) return a;
+	return group[a] = Find(group[a]);
+}
+
+void Union(int a, int b) {
+	group[Find(a)] = Find(b); // 각 루트를 서로 합침
+}
 
 int main() {
-	cin >> city >> schedule;
-	
-	for (int k = 0; k < city; k++) {
-		for (int i = 0; i < city; i++) {
-			// NxN 번 만큼 루프를 돌며 연결여부를 입력받음
+	cin >> N >> M;
+
+	for (int k = 1; k <= N; k++) {
+		group[k] = k; // 자기 본인 값이면 루트
+	}
+
+	for (int k = 1; k <= N; k++) {
+		for (int m = 1; m <= N; m++) {
 			int a;
 			cin >> a;
-			if (a)  // 연결 여부는  0 또는 1로 입력되므로, 1인 경우 참
-				if (k <= i) { // 이때, 연결은 대칭성을 띄게 됨
-					// so, 중복입력을 피하기 위해 행렬의 크기 정보를 비교
-					Link* find = &Bridge[i];
-					while (find->link != NULL) { // 빈 칸을 찾을 떄 까지 계속 돌림
-						find = find->link; //해당 칸으로 이동
-					}
-					find->link = &Bridge[k]; // i부터 거슬러 올라간 노드에서 k로 연결
-				}
-		}
-	} // 그래프 생성
-
-	int to = -1; // 한 번에 두개의 노드만을 비교할 예정
-	Link* from = NULL, *find = NULL;
-	while (schedule--) {
-		from = find; // 이전 정보를 저장
-		cin >> to;
-
-		find = &Bridge[to - 1];
-		while (find->link != NULL) { // 빈 칸을 찾을 떄 까지 계속 돌림
-			find = find->link; //해당 칸으로 이동
-		}
-
-		if (from != NULL) { // 첫 입력이 아니라면
-			if (from != find) { // 비교하는 두 대상이 연결되어있지 않으면 절대 불가능
-				cout << "NO" << endl;
-				return 0;
-			}
+			if (a) Union(k, m); // k와 m은 같은 그룹 소속 (연결됨)
 		}
 	}
 
-	cout << "YES" << endl; // 반복문을 무사히 나온 경우 성공
-	return 0;
-}
-
-Link::Link() {
-	link = NULL;
+	bool flag = true;
+	int start;
+	cin >> start;
+	for (int k = 1; k < M; k++) {
+		int idx;
+		cin >> idx;
+		if (Find(start) != Find(idx)) { // 그룹이 다르다면 해당 경로는 불가능
+			flag = false;
+			break;
+		}
+	}
+	cout << (flag ? "YES" : "NO") << endl;
 }
